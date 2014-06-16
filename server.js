5034f0a2cec2e536e46738bd085a40dbadfed2d2
Dmitry White <white.dmitry@gmail.com>
2014-06-16 16:08:05 -0700

    Server.js will find the file if a correct path is provided, but will not accept requests for favicons, etc

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 // This is the actual server code application thingy dingy
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+var server = http.createServer(function (req, res) {
+  console.log(__dirname);
+  console.log(req.url);
+  var resourcePath = path.resolve(__dirname + req.url);
+  fs.createReadStream(resourcePath, {encoding: 'utf8'}).pipe(res);
+});
+
+console.log('the server is listening');
+server.listen(8000);
