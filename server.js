f7aab784420b9e4210fa61fc505e6634b8246bee
J. Allen Tucker <j.allentucker@gmail.com>
2014-06-16 17:04:34 -0700

    almost to handling 404 errors

--- server.js
+++ server.js
@@ -6,12 +6,16 @@ var path = require('path');
 var server = http.createServer(function (req, res) {
   if (path.extname(req.url) === '.ico') { return res.end(); }
   var resourcePath = path.resolve(__dirname + req.url);
-  if (path.extname(req.url)) {
-    fs.createReadStream(resourcePath, {encoding: 'utf8'}).pipe(res);
-  } else {
-    fs.createReadStream(path.resolve(resourcePath + '/index.html'),
-      {encoding: 'utf8'}).pipe(res);
-  }
+  var readStream = fs.createReadStream(resourcePath);
+  var evaluateFile = function() {
+    readStream.on('open', function() { readStream.pipe(res); });
+    readStream.on('error', function(err) { res.end(err); });
+  };
+  if (path.extname(req.url)) { evaluateFile();
+  } else if (path.extname(req.url) === '') {
+    resourcePath = (path.resolve(resourcePath + '/index.html'));
+      evaluateFile();
+  } else { res.writeHead(404); }
 });
 
 console.log('the server is listening');
